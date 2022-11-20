6f59eb04c316caa93bc93ae4328485e8c43c5edb
Jamzmarks <jamzmarks@gmail.com>
2022-11-20 16:32:21 -0300

    SENSOR ULTRASONICO

--- RoboSeguidor.java
+++ RoboSeguidor.java
@@ -18,12 +18,31 @@ James Marques N9508A0
 #define potencia_dir 9
 #define potencia_esq 10
 
-int ValorCorte = 700; //Valor de Corta 700: Linha Preta 300: Linha Branca
+
+
+//Incluindo biblioteca Ultrasonic.h
+#include "Ultrasonic.h"
+
+//Criando objeto ultrasonic e definindo as portas digitais
+//do Trigger - 0 - e Echo - 1
+Ultrasonic SensorUltrassonico1(0, 1);
+
+
+
+//CRIA O OBJETO ULTRASONIC E DEFINE OS PINOS PARA O TRIGGER(0) E ECHO(1)(SENSOR DE DISTÂNCIA1152Ultrasonic SensorUltrassonico1(0, 1);
+
+long Microsegundos = 0;// Variável para armazenar o valor do tempo da reflexão do som refletido pelo objeto fornecido pela biblioteca do sensor
+float DistanciaemCM = 0;// Variável para armazenar o valor da distância a ser convertido por uma função da própria bilbioteca do sensor
+
+int ValorCorte = 300; //Valor de Corta 700: Linha Preta 300: Linha Branca
 const int velocidade = 150; //Velocidade do carrinho
+
+
+
 
 void setup (){
 
-  Serial.begin(5000); //INICIAR APÓS 5 SEGUNDOS
+  Serial.begin(115200); 
   pinMode(motor_esquerdo_re, OUTPUT);
   pinMode(mEsquerdo, OUTPUT);
   pinMode(motor_direito_re, OUTPUT);
@@ -34,8 +53,8 @@ void setup (){
 
   pinMode(SE, INPUT);
   pinMode(SD, INPUT);
-  delay(5000)
-  
+
+  delay(5000);// Tempo de espera para inicialização   
 }
 
 void loop(){
@@ -55,24 +74,26 @@ void loop(){
   // bool VLE = !(L_Sensor > ValorCorte);
 
   //Define um valor booleano para os sensores 
-  bool VLD = R_Sensor > ValorCorte;
-  bool VLE = L_Sensor > ValorCorte;
+  bool VLD = R_Sensor < ValorCorte;
+  bool VLE = L_Sensor < ValorCorte;
 
+  //Convertendo a distância em CM e lendo o sensor
+  DistanciaemCM = SensorUltrassonico1.convert(SensorUltrassonico1.timing(), Ultrasonic::CM);
 
-  if(!(VLE)&&!(VLD)){
+  Serial.print(DistanciaemCM);
+  Serial.println(" cm");
 
-    analogWrite (potencia_dir,0); //PARA     
-    analogWrite (potencia_esq,0);
-    delay(500);
-    // if(!(VLE)&&!(VLD)){
-    //   analogWrite (potencia_dir,velocidade);  //VELOCIDADE    
-    //   analogWrite (potencia_esq,velocidade);  //VELOCIDADE
-    //   digitalWrite(motor_esquerdo_re,HIGH); //SENTIDO DE ROTACAO
-    //   digitalWrite(motor_direito_re,HIGH); //SENTIDO DE ROTACAO
-    // } 
-  }else{
+  if (DistanciaemCM <= 5) {// Se a distância lida pelo sensor for ou igual a 5 centimetros
+    Serial.print("obstaculo")
+  }
+  }else{ //FRENTE
+    if(!(VLE)&&!(VLD)){
 
-    //FRENTE
+      analogWrite (potencia_dir,0); //PARA     
+      analogWrite (potencia_esq,0);
+      delay(500);
+
+    }
     if((VLE)&&(VLD)){
       analogWrite (potencia_dir,velocidade);    
       analogWrite (potencia_esq,velocidade); 
@@ -99,6 +120,8 @@ void loop(){
       Serial.print("ESQUERDA \n"); //TESTE CONSOLE
       // delay(500); 
     }
-    
-   }
 }
+
+  
+
+  
